9ee1e319d451efd7131011e2533d1f3f49458e20
Bulat Lutfullin <lb6557@gmail.com>
2017-06-30 13:07:09 +0300

    Gateway CORS support

---
+++ envoy/gateway/src/main/java/io/hydrosphere/serving/config/CORSConfig.java
@@ -0,0 +1,30 @@
+package io.hydrosphere.serving.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by bulat on 30.06.17.
+ */
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .maxAge(3600)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
