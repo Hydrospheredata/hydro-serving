dd193fd1af27eda7675e37f2b4a15c5e772b8bca
Zajs <aga.zajs@gmail.com>
2017-10-10 18:04:03 +0300

    - zipkin fix

--- manager/src/main/docker/start.sh
+++ manager/src/main/docker/start.sh
@@ -22,12 +22,12 @@ SERVICE_ID=$1
 
 JAVA_OPTS="-Xmx$JAVA_XMX -Xms$JAVA_XMX"
 
-APP_OPTS="-DopenTracing.zipkin.enabled=$ZIPKIN_ENABLED -DopenTracing.zipkin.port=$ZIPKIN_PORT -DopenTracing.zipkin.host=$ZIPKIN_HOST "
-APP_OPTS="$APP_OPTS -Dapplication.port=$APP_HTTP_PORT -Dapplication.appId=$SERVICE_ID"
+APP_OPTS="-Dapplication.port=$APP_HTTP_PORT -Dapplication.appId=$SERVICE_ID"
 APP_OPTS="$APP_OPTS -Dsidecar.port=$SIDECAR_HTTP_PORT"
 
 if [ "$CUSTOM_CONFIG" = "" ]
 then
+    APP_OPTS="-DopenTracing.zipkin.enabled=$ZIPKIN_ENABLED -DopenTracing.zipkin.port=$ZIPKIN_PORT -DopenTracing.zipkin.host=$ZIPKIN_HOST"
     APP_OPTS="$APP_OPTS -Dmanager.advertisedHost=$ADVERTISED_MANAGER_HOST -Dmanager.advertisedPort=$ADVERTISED_MANAGER_PORT"
     APP_OPTS="$APP_OPTS -Ddatabase.jdbcUrl=jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME"
     APP_OPTS="$APP_OPTS -Ddatabase.username=$DATABASE_USERNAME -Ddatabase.password=$DATABASE_PASSWORD"
