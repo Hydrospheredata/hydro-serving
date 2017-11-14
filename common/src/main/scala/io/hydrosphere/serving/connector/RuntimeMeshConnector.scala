844089c546a4bf7bdf5d41e966cb32cacd3d6800
dos65 <qtankle@gmail.com>
2017-11-14 18:49:40 +0300

    Zipkin tracing - try latest envoy, send model requests over additional envoy listener (don't sure that all off that is required)

--- common/src/main/scala/io/hydrosphere/serving/connector/RuntimeMeshConnector.scala
+++ common/src/main/scala/io/hydrosphere/serving/connector/RuntimeMeshConnector.scala
@@ -53,7 +53,7 @@ class HttpRuntimeMeshConnector(config: SidecarConfig)(
 
   //TODO ConnectionPool
   //TODO Move to streams
-  val flow = Http(system).outgoingConnection(config.host, config.port)
+  val flow = Http(system).outgoingConnection("localhost", 9292)
     .mapAsync(1) { r =>
       r.entity.toStrict(10.seconds).map(entity => {
         val data = entity.data.toArray
@@ -67,26 +67,27 @@ class HttpRuntimeMeshConnector(config: SidecarConfig)(
     }
 
   private def execute(runtimeName: String, runtimePath: String, headers: Seq[HttpHeader], json: Array[Byte]): Future[ExecutionResult] = {
+    val headers2 = collection.immutable.Seq(headers: _*)
     val httpRequest=HttpRequest(
       uri = runtimePath,
       method = HttpMethods.POST,
       entity = HttpEntity.apply(ContentTypes.`application/json`, json),
-      headers = collection.immutable.Seq(headers: _*) :+ Host.apply(runtimeName)
+      headers = headers2 :+ Host.apply(runtimeName)
     )
     val source = Source.single(httpRequest)
     source.via(flow).runWith(Sink.head)
   }
 
-  private def getParentSpanId(headers: Seq[HttpHeader]): Seq[HttpHeader] = {
-    headers.find(_.name() == TracingHeaders.xB3SpanId).map(h => {
-      RawHeader(TracingHeaders.xB3ParentSpanId, h.value())
-    }).toSeq
-  }
+//  private def getParentSpanId(headers: Seq[HttpHeader]): Seq[HttpHeader] = {
+//    headers.find(_.name() == TracingHeaders.xB3SpanId).map(h => {
+//      RawHeader(TracingHeaders.xB3ParentSpanId, h.value())
+//    }).toSeq
+//  }
 
   override def execute(command: ExecutionCommand): Future[ExecutionResult] = {
     val empty = ExecutionSuccess(
       command.json,
-      command.headers ++ getParentSpanId(command.headers),
+      command.headers,
       StatusCodes.OK
     )
 
