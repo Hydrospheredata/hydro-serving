ab40141c49e7427df5b2d6f64848b51db9bb8169
Bulat <lb6557@gmail.com>
2017-12-08 16:43:08 +0300

    Fetcher test fix

--- manager/src/test/scala/io/hydrosphere/serving/manager/service/modelfetcher/FetcherSpecs.scala
+++ manager/src/test/scala/io/hydrosphere/serving/manager/service/modelfetcher/FetcherSpecs.scala
@@ -7,24 +7,24 @@ import io.hydrosphere.serving.model_api.ModelType
 import org.scalatest._
 
 class FetcherSpecs extends FlatSpec with Matchers {
-  val localSource = new LocalModelSource(LocalModelSourceConfiguration("test", TestConstants.localModelsPath))
+  val localSource = new LocalModelSource(LocalModelSourceConfiguration("test_models", TestConstants.localModelsPath))
 
   "Scikit model fetcher" should "parse correct scikit model" in {
     val model = ScikitModelFetcher.fetch(localSource, "scikit_model")
     model shouldBe defined
-    assert(model.get.modelType === ModelType.Scikit)
+    assert(model.get.modelType === ModelType.Scikit())
   }
 
   "Spark model fetcher" should "parse correct spark model" in {
     val model = SparkModelFetcher.fetch(localSource, "spark_model")
     model shouldBe defined
-    assert(model.get.modelType === ModelType.Spark("2.1.0"))
+    assert(model.get.modelType === ModelType.Spark("2.1.1"))
   }
 
   "Tensorflow model fetcher" should "parse correct tensorflow model" in {
     val model = TensorflowModelFetcher.fetch(localSource, "tensorflow_model")
     model shouldBe defined
-    assert(model.get.modelType === ModelType.Tensorflow)
+    assert(model.get.modelType === ModelType.Tensorflow())
   }
 
   "Fetcher chain" should "parse source" in {
