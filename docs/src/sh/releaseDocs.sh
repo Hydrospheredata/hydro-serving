33c59535d840ae2d19d01bded50c78c0e86b2aa4
Bulat <lb6557@gmail.com>
2019-04-04 17:31:03 +0300

    Fix for duplicates

--- docs/src/sh/releaseDocs.sh
+++ docs/src/sh/releaseDocs.sh
@@ -2,5 +2,5 @@
 
 ln -Fs ~/serving_publish_dir_new/$1 ~/serving_publish_dir_new/latest
 cp ~/serving_publish_dir_new/latest/paradox.json ~/serving_publish_dir_new/paradox.json
-jq '.[. | length] |= . + "'$1'"' ~/serving_publish_dir_new/versions.json > ~/serving_publish_dir_new/versions_new.json
+jq 'map(select( . != "'$1'")) | .[. | length] |= . + "'$1'"' ~/serving_publish_dir_new/versions.json > ~/serving_publish_dir_new/versions_new.json
 mv ~/serving_publish_dir_new/versions_new.json  ~/serving_publish_dir_new/versions.json
