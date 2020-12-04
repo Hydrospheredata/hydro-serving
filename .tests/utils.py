78b3b22546ff2cf3e25e635d31617cb2486aa2df
beardedwhale <Lisch.batanina@gmail.com>
2020-12-04 15:28:25 +0300

    * Fix wrong environment variable

--- .tests/utils.py
+++ .tests/utils.py
@@ -1,19 +1,15 @@
 import json
-import os
-from enum import Enum
 
 import pandas as pd
 import requests
-from config import *
-from hydrosdk import LocalModel, DockerImage
 from hydroserving.core.model.parser import parse_model
 from hydroserving.util.yamlutil import yaml_file
 
-CLUSTER_URL = os.getenv("CLUSTER_URL", 'http://localhost:80')
+from config import *
 
 
 def get_production_subsample(model_id, size=1000) -> pd.DataFrame:
-    url = f'{CLUSTER_URL}/monitoring/checks/subsample/{model_id}?size={size}'
+    url = f'{HTTP_PROXY_ADDRESS}/monitoring/checks/subsample/{model_id}?size={size}'
     r = requests.get(url)
     if r.status_code == 200:
         return pd.DataFrame.from_dict(r.json())
@@ -31,6 +27,6 @@ def read_model_conf(model_root):
 
 
 def get_training_data_path(modelversion_id):
-    response = requests.get(f'{CLUSTER_URL}/monitoring/training_data?modelVersionId={modelversion_id}')
+    response = requests.get(f'{HTTP_PROXY_ADDRESS}/monitoring/training_data?modelVersionId={modelversion_id}')
     training_data_s3 = json.loads(response.text)
     return training_data_s3
